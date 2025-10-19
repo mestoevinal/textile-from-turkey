3a95557a534f0c637d133f53064420212b72cb7e
Inal Mestoev <mestoevinal@mail.ru>
2025-10-19 17:37:39 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { PageRenderer } from './pages/Renderer'
 import { clientData, clientPage } from './stubData/test'
+import '../index.css'
 
 export function App() {
   return <PageRenderer config={clientPage as unknown as any} data={clientData} />
