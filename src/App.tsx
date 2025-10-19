22f8c084d8d86a773344cd7f37416f0cca9ea96f
Inal Mestoev <mestoevinal@mail.ru>
2025-10-19 17:11:59 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { PageRenderer } from './pages/Renderer'
+import { clientData, clientPage } from './stubData/test'
+
 export function App() {
-  return (
-    <div>
-      Telegram CRM constuctor
-    </div>
-  )
+  return <PageRenderer config={clientPage as unknown as any} data={clientData} />
 }
