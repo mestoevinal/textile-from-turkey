190faa3c5c7feb747dd4fbf5e1768656eceae9d3
Inal Mestoev <mestoevinal@mail.ru>
2025-10-22 21:40:58 +0300

    fix

--- src/components/RenderSwitch.tsx
+++ src/components/RenderSwitch.tsx
@@ -3,7 +3,7 @@ import React, { type JSX } from 'react';
 type RenderSwitchProps = {
   condition: boolean;
   whenTrue: JSX.Element;
-  whenFalse: JSX.Element;
+  whenFalse: JSX.Element | null;
 };
 
 export const RenderSwitch: React.FC<RenderSwitchProps> = ({ condition, whenTrue, whenFalse }) => {
