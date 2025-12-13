4bdbb8837fe5001a4d5a1d50eaef0781cd6ac5c2
Inal Mestoev <mestoevinal@mail.ru>
2025-12-13 23:48:29 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,18 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { createFilter, defineConfig, transformWithEsbuild, type Plugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { readFileSync } from 'fs';
+import svgr from 'vite-plugin-svgr' // ← Готовый плагин
 
-function svgrPlugin(): Plugin {
-  const filter = createFilter('**/*.svg');
-  const postfixRE = /[?#].*$/s;
-
-  return {
-    name: 'svgr-plugin',
-    async transform(code: any, id: any) {
-      if (filter(id)) {
-        const { transform } = await import('@svgr/core');
-        const { default: jsx } = await import('@svgr/plugin-jsx');
-
-        const filePath = id.replace(postfixRE, '');
-        const svgCode = readFileSync(filePath, 'utf8');
-
-        const componentCode = await transform(svgCode, undefined, {
-          filePath,
-          caller: {
-            previousExport: code,
-            defaultPlugins: [jsx],
-          },
-        });
-
-        const res = await transformWithEsbuild(componentCode, id, {
-          loader: 'jsx',
-        });
-
-        return {
-          code: res.code,
-          map: null,
-        };
-      }
-    },
-  };
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgrPlugin(), react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: 'named', // ← Поддержка { ReactComponent }
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
+  ],
 })
