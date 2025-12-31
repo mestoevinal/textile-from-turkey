c15d8a3c7b423df620ab3dc1cf6831ffe01e42ea
Inal Mestoev <mestoevinal@mail.ru>
2025-12-31 15:47:59 +0300

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { AppSidebar } from './sidebar/sidebar'
-import { PageRenderer } from './pages/Renderer'
-import { clientData, clientPage, productData, productPage } from './stubbed-data/client'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import Papa from 'papaparse';
 
+const SHEET_ID = '1FjeC0gX-2rDkh0SX-0EVPBhCKf2Sgt3Rzw8EzE3sMc8';
+const url = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv`;
+
+async function fetchProducts() {
+  const response = await fetch(url);
+  const text = await response.text();
+  
+  const { data } = Papa.parse(text, { header: true });
+  return data;
+}
 export function App() {
+  
+  const [products, setProducts] = useState([]);
+  console.log('🚀 ~ App ~ products:', products)
   
   useEffect(() => {
     const tg = (window as any).Telegram?.WebApp;
@@ -14,15 +26,18 @@ export function App() {
       tg.expand();
     }
   }, []);
+  
+  useEffect(() => {
+    fetchProducts().then(data => setProducts(data as any));
+  }, []);
 
   return (
     <BrowserRouter>
         <div className="min-h-screen flex bg-gray-50">
-          <AppSidebar />
           <main className="flex-1 lg:ml-64 p-4 lg:p-6">
             <Routes>
-              <Route path="/client" element={ <PageRenderer config={clientPage as any} data={clientData}/> } />
-              <Route path="/products" element={ <PageRenderer config={productPage as any} data={productData}/>  } />
+              {/* <Route path="/client" element={ <PageRenderer config={clientPage as any} data={clientData}/> } />
+              <Route path="/products" element={ <PageRenderer config={productPage as any} data={productData}/>  } /> */}
             </Routes>
           </main>
         </div>
